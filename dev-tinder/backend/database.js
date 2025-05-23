ed0d59dd0e2a43be8455aefbef1d38fa235ebbb7
Suyash Mishra <suyash.mishra@cavisson.com>
2025-05-23 07:35:18 +0530

    data sanitization and validation

--- dev-tinder/backend/database.js
+++ dev-tinder/backend/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     await mongoose.connect(
-        'mongodb+srv://suyashmishra20003:0191Ex131104@suyashcluster0.7mij7o1.mongodb.net/devTinder'
+        ''
     )
 }
 
